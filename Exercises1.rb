5378416547300a98c034d072e44459b5c3d7c3ce
Eduardo <eduardotejedadelgado@gmail.com>
2024-01-10 02:02:15 -0400

    Mas ejemplos

---
+++ Exercises1.rb
@@ -0,0 +1,11 @@
+# 1
+# Use the code in listings 1.3 and 1.4 to create c2fio.rb, a program that reads a
+# number from a file and writes the Fahrenheit conversion to a different file. The
+# resulting program will print the following output, and temp.out will contain the
+# Fahrenheit equivalent of the value in temp.dat:
+# Reading Celsius temperature value from data file...
+# Saving result to output file 'temp.out'
+
+# Reading Celsius temperature value from data file...
+# Saving result to output file 'temp.out'
+
