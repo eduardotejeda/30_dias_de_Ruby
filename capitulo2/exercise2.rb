cd5ab3a65e5a575ac343293ebd168c87ff5abb00
Eduardo <eduardotejedadelgado@gmail.com>
2024-01-13 08:24:53 -0400

    Respuesta al ejercicio 2.4

--- capitulo2/exercise2.rb
+++ capitulo2/exercise2.rb
@@ -22,9 +22,22 @@ def ticket.venue
     def ticket.price
         5.50
 end
+
 def ticket.print_details(*x)
 x.each { |detail| puts "This ticket is #{detail}" }
 end
+
+print "This ticket is for: "
+print ticket.event + ", at "
+print ticket.venue + ", on "
+puts ticket.date + "."
+print "The performer is "
+puts ticket.performer + "."
+print "The seat is "
+print ticket.seat + ", "
+print "and it costs $"
+puts "%.2f." % ticket.price
+ticket.print_details("non-refundable", "non-transferable", "in a non-smoking section")
 
 # Add the ticket details (displayed in bold) to the output:
 # This ticket is for: Author's reading, at Town Hall, on 1903-01-02.
