64f7b7dd5542a907fe0552578376d0026c3fc3b0
Eduardo <eduardotejedadelgado@gmail.com>
2024-01-16 20:34:03 -0400

    equal as filters

--- capitulo3/equalsign.rb
+++ capitulo3/equalsign.rb
@@ -14,3 +14,19 @@ end
 ticket = Ticket.new("Mi Casa","12-5-2014")
 ticket.price= 56.4
 puts "#{ticket.price}"
+
+#Equal sign methods can also serve as filters or gatekeepers.
+
+class Ticket
+    def price=(amount)
+        f (amount * 100).to_i == amount * 100
+@price = amount
+else
+puts "The price seems to be malformed"
+end
+end
+def price
+@price
+end
+end
+
