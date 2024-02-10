dc396293526817d8aec56def7e5fefe0ef0eb04c
Eduardo <eduardotejedadelgado@gmail.com>
2024-02-10 17:53:43 -0400

    Comentario a prepend

--- capitulo4/prepend.rb
+++ capitulo4/prepend.rb
@@ -1,3 +1,5 @@
+#prepend si quiero que los metodos dentro del modulo tengan prioridad sobre los metodos en la clase con el mismo nombre
+
 module MeFirst
     def report
     puts "Hello from module!"
@@ -11,3 +13,4 @@ module MeFirst
     end
     p = Person.new
     p.report
+    puts Person.ancestors #te permite ver el orden del anidado de los objetos
